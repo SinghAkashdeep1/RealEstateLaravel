2785cb6aaeb4fe38218d9ff30efab104b96087a2
SinghAkashdeep1 <singhakashdeep1@seasiaconnect.com>
2024-08-26 18:59:11 +0530

    add forgot pass api and profile

--- app/Http/Controllers/Api/Admin/AdminForgotPasswordController.php
+++ app/Http/Controllers/Api/Admin/AdminForgotPasswordController.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
+use App\Models\User;
 use DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminForgotPasswordController extends Controller
 {
